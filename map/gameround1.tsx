8d31e70ee592dd32d070e8d5c15ea6335cb8beca
silkkang <lsm000509@naver.com>
2025-07-23 14:21:36 +0900

    xenoTacticProject-bin

---
+++ map/gameround1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="gameround1" tilewidth="32" tileheight="32" tilecount="315" columns="21">
+ <image source="gameround1.png" width="672" height="504"/>
+</tileset>
---
+++ map/gameui.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="gameui" tilewidth="32" tileheight="32" tilecount="315" columns="21">
+ <image source="gameui.png" width="672" height="504"/>
+</tileset>
